09cdf15e98a43b5fad892bbf554bb6e03b1d0ab5
Rahul Paul <rahulpaul.iitkgp@gmail.com>
2016-08-29 12:34:06 +0530

    Update prob5.py
    Improved solution

--- dp/prob5.py
+++ dp/prob5.py
@@ -57,56 +57,76 @@ __author__ = 'rahul'
 #Returns: 2926
 
 
-class BadNeighbors(object):
 
-    @staticmethod
-    def max_donations_internal(donations):
-        solution = {}
 
-        for i, e in enumerate(donations):
+from collections import namedtuple
 
-            if i == 0:
-                solution[i] = donations[0], {0}
-                continue
 
-            if i == 1:
-                solution[i] = (donations[0], {0}) if donations[0] > donations[1] else (donations[1], {1})
-                continue
+Solution = namedtuple('Solution', ['total_donation', 'donators'])
 
-            new_opt1 = solution[i-1][0]
-            new_opt_set1 = solution[i-1][1]
 
-            new_opt2 = solution[i-2][0] + donations[i]
-            new_opt_set2 = set(solution[i-2][1])
-            new_opt_set2.add(i)
+def _compute_max_donation_ij(donations, j_solution, i, i_donation, is_i_last):
+    """
+    Returns the max possible donation including i
+    """
+    if (0 in j_solution.donators) and is_i_last:
+        return j_solution.total_donation - donations[0] + i_donation
+    return j_solution.total_donation + i_donation
 
-            solution[i] = (new_opt1, new_opt_set1) if new_opt1 > new_opt2 else (new_opt2, new_opt_set2)
 
-        return max(solution.itervalues(), key=lambda x: x[0])
+def _compute_max_solution_ij(donations, j_solution, i, i_donation, is_i_last):
+    """
+    Returns the optimal solution including i
+    """
+    if (0 in j_solution.donators) and is_i_last:
+        return Solution(j_solution.total_donation - donations[0] + i_donation, j_solution.donators[1:] + [i])
+    return Solution(j_solution.total_donation + i_donation, j_solution.donators + [i])
 
-    @staticmethod
-    def max_donations(donations):
-        r1 = BadNeighbors.max_donations_internal(donations[:-1])
-        r2 = BadNeighbors.max_donations_internal(donations[1:])
-        return max((r1, r2), key=lambda x: x[0])
+
+def max_donations(donations):
+    solutions_dict = {}
+    for i, donation in enumerate(donations):
+        if i - 2 < 0:
+            solutions_dict[i] = Solution(donation, [i])
+        else:
+            i_solution = Solution(donation, [i])
+            is_i_last = (i == len(donations) - 1)
+            for j in range(i-1):
+                ij_donation = _compute_max_donation_ij(donations, solutions_dict[j], i, donation, is_i_last)                
+                if ij_donation > i_solution.total_donation:                    
+                    i_solution = _compute_max_solution_ij(donations, solutions_dict[j], i, donation, is_i_last)
+            solutions_dict[i] = i_solution
+    return max(solutions_dict.values(), key=lambda soln : soln.total_donation)
 
 
 def main():
-    d1 = 10, 3, 2, 5, 7, 8
-    print BadNeighbors.max_donations(d1)
+    sep = '======================================================='
+    d1 = [10, 3, 2, 5, 7, 8]
+    print('expected optimal donation = {}\ndonations = {}'.format(19, d1))
+    print(max_donations(d1))
+    print(sep)
 
-    d2 = 11, 15
-    print BadNeighbors.max_donations(d2)
+    d2 = [11, 15]
+    print('expected optimal donation = {}\ndonations = {}'.format(15, d2))
+    print(max_donations(d2))
+    print(sep)
 
-    d3 = 7, 7, 7, 7, 7, 7, 7
-    print BadNeighbors.max_donations(d3)
+    d3 = [7, 7, 7, 7, 7, 7, 7]
+    print('expected optimal donation = {}\ndonations = {}'.format(21, d3))
+    print(max_donations(d3))
+    print(sep)
 
-    d4 = 1, 2, 3, 4, 5, 1, 2, 3, 4, 5
-    print BadNeighbors.max_donations(d4)
+    d4 = [1, 2, 3, 4, 5, 1, 2, 3, 4, 5]
+    print('expected optimal donation = {}\ndonations = {}'.format(16, d4))
+    print(max_donations(d4))
+    print(sep)
 
-    d5 = 94, 40, 49, 65, 21, 21, 106, 80, 92, 81, 679, 4, 61, 6, 237, 12, 72, 74, 29, 95, 265, 35, 47, 1, 61, 397, 52, \
-         72, 37, 51, 1, 81, 45, 435, 7, 36, 57, 86, 81, 72
-    print BadNeighbors.max_donations(d5)
+    d5 = [94, 40, 49, 65, 21, 21, 106, 80, 92, 81, 679, 4, 61, 6, 237, 12, 72, 74, 29, 95, 
+          265, 35, 47, 1, 61, 397, 52, 72, 37, 51, 1, 81, 45, 435, 7, 36, 57, 86, 81, 72]
+    print('expected optimal donation = {}\ndonations = {}'.format(2926, d5))
+    print(max_donations(d5))
+    print(sep)
+
 
 
 if __name__ == '__main__':
