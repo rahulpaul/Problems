ff7de926f94106c1afe4fcb58193f4ea906fd76d
Rahul Paul <rahulpaul.iitkgp@gmail.com>
2019-02-16 22:56:41 +0530

    Update fraud_activity.py

--- hackerrank/fraud_activity.py
+++ hackerrank/fraud_activity.py
@@ -1,116 +1,135 @@
 import sys
+import math
 from typing import List
 from collections import deque, Counter
 
 
-def detect_frauds(transactions: List[int], duration: int):
-    mintxn, maxtxn = sys.maxsize, -1
-    for txn in transactions:
-        if txn < mintxn:
-            mintxn = txn
-        if txn > maxtxn:
-            maxtxn = txn
+class FraudDetector:
 
-    bin_size = 1
-    min_window = mintxn // bin_size
-    max_window = maxtxn // bin_size
+    def __init__(self, transactions):
+        self.transactions = transactions
+        self.bin_size = 20
 
-    bin_count = max_window - min_window + 1
-    bin_list: List[Counter] = [Counter() for _ in range(bin_count)]
-    bin_item_counts = [0 for _ in range(bin_count)]
+        mintxn, maxtxn = sys.maxsize, -1
+        for txn in transactions:
+            if txn < mintxn:
+                mintxn = txn
+            if txn > maxtxn:
+                maxtxn = txn
 
-    history = deque()
+        self.min_window = int(math.floor(mintxn / self.bin_size))
+        self.max_window = int(math.ceil(maxtxn / self.bin_size))
 
-    c = ((duration + 1) // 2, ) if duration % 2 != 0 else (duration//2, duration//2 + 1)
-    fraud_count = 0
+        self.bin_count = self.max_window - self.min_window + 1
+        self.bin_list: List[Counter] = [Counter() for _ in range(self.bin_count)]
+        self.bin_item_counts = [0 for _ in range(self.bin_count)]
 
-    for txn in transactions:
+        self.history = deque()
 
-        txn_bin_index = txn // bin_size - min_window
+    def compute_bin_index(self, value):
+        # return int(math.ceil(value / self.bin_size)) - self.min_window
+        return value // self.bin_size - self.min_window
 
-        if len(history) < duration:
-            history.append(txn)
-            bin_list[txn_bin_index][txn] += 1
-            bin_item_counts[txn_bin_index] += 1
-            continue
+    def add_to_history(self, transaction):
+        self.history.append(transaction)
+        transaction_bin_index = self.compute_bin_index(transaction)
+        self.bin_list[transaction_bin_index][transaction] += 1
+        self.bin_item_counts[transaction_bin_index] += 1
 
-        value = txn / 2
-        value_bin_index = value // bin_size - min_window
-        left, right = 0, 0
+    def pop_from_history(self):
+        transaction = self.history.popleft()
+        transaction_bin_index = self.compute_bin_index(transaction)
+        self.bin_list[transaction_bin_index][transaction] += -1
+        self.bin_item_counts[transaction_bin_index] += -1
 
-        for index, item_count in enumerate(bin_item_counts):
-            right = left + item_count
-            if left < c[0] < right:
-                # this bin contains the median
-                if value_bin_index > index:
-                    fraud_count += 1
-                    break
-                if value_bin_index == index:
-                    # both value and median is in this bin
-                    # we can't decide if fraud or not, need to compute the median
-                    # print("Computing median")
-                    counter = bin_list[index]
-                    sorted_keys = sorted(counter.keys())
+    def detect_frauds(self, duration: int):
+
+        c = ((duration + 1) // 2, ) if duration % 2 != 0 else (duration//2, duration//2 + 1)
+        fraud_count = 0
+        frauds = []
+
+        for txn_index, txn in enumerate(self.transactions):
+
+            if len(self.history) < duration:
+                self.add_to_history(txn)
+                continue
+
+            value = txn / 2
+            value_bin_index = self.compute_bin_index(value)
+            left, right = 0, 0
+
+            for index, item_count in enumerate(self.bin_item_counts):
+                right = left + item_count
+                if left < c[0] < right:
+                    # this bin contains the median
+                    if value_bin_index > index:
+                        fraud_count += 1
+                        frauds.append(txn_index)
+                        break
+                    if value_bin_index == index:
+                        # both value and median is in this bin
+                        # we can't decide if fraud or not, need to compute the median
+                        # print("Computing median")
+                        counter = self.bin_list[index]
+                        sorted_keys = sorted(counter.keys())
+                        median = -1
+
+                        for key_index, key in enumerate(sorted_keys):
+                            left += counter[key]
+                            if left > c[0]:
+                                median = key
+                                break
+                            elif left == c[0] and len(c) == 1:
+                                median = key
+                                break
+                            elif left == c[0] and len(c) == 2:
+                                next_key = sorted_keys[key_index + 1]
+                                median = (key + next_key) / 2
+                                break
+
+                        if value >= median:
+                            fraud_count += 1
+                            frauds.append(txn_index)
+                        break
+
+                elif c[0] == right:
+                    counter = self.bin_list[index]
                     median = -1
-
-                    for key in sorted_keys:
-                        left += counter[key]
-                        if left > c[0]:
-                            median = key
-                            break
-                        elif left == c[0] and len(c) == 1:
-                            median = key
-                            break
-                        elif left == c[0] and len(c) == 2:
-                            next_bin_counter = bin_list[index + 1]
-                            min_key = min(next_bin_counter.keys())
-                            median = (key + min_key) / 2
-                            break
+                    if len(c) == 1:
+                        median = max(counter.keys())
+                    elif len(c) == 2:
+                        counter2 = self.bin_list[index + 1]
+                        m1 = max(counter.keys())
+                        m2 = min(counter2.keys())
+                        median = (m1 + m2) / 2
 
                     if value >= median:
                         fraud_count += 1
+                        frauds.append(txn_index)
+
                     break
 
-            elif c[0] == right:
-                counter = bin_list[index]
-                median = -1
-                if len(c) == 1:
-                    median = max(counter.keys())
-                elif len(c) == 2:
-                    counter2 = bin_list[index + 1]
-                    m1 = max(counter.keys())
-                    m2 = min(counter2.keys())
-                    median = (m1 + m2) / 2
+                elif value_bin_index == index:
+                    break
 
-                if value >= median:
-                    fraud_count += 1
+                left = right
 
-                break
+            # remove from history
+            self.pop_from_history()
 
-            elif value_bin_index == index:
-                break
+            # add to history
+            self.add_to_history(txn)
 
-            left = right
-
-        # remove from history
-        item_to_remove = history.popleft()
-        item_to_remove_index = item_to_remove // bin_size - min_window
-        bin_item_counts[item_to_remove_index] += -1
-        item_to_remove_counter = bin_list[item_to_remove_index]
-        item_to_remove_counter[item_to_remove] += -1
-
-        # add to history
-        history.append(txn)
-        bin_list[txn_bin_index][txn] += 1
-        bin_item_counts[txn_bin_index] += 1
-
-    return fraud_count
+        # print(frauds)
+        return fraud_count
 
 
 def main():
     n, d = (int(x) for x in input().split(' '))
     transactions = [int(x) for x in input().split(' ')]
-    print(detect_frauds(transactions, d))
+
+    fraud_detector = FraudDetector(transactions)
+    print(fraud_detector.detect_frauds(d))
 
 
 if __name__ == "__main__":
