4783c03a4ac2810a92e22268fa631e5825e9b7d5
rahpaul <rahul.p.paul@oracle.com>
2020-09-05 11:40:28 +0530

    added max_product_subarray.py

---
+++ leetcode/interview/google/max_product_subarray.py
@@ -0,0 +1,36 @@
+""" https://leetcode.com/problems/maximum-product-subarray/
+
+Given an integer array nums, find the contiguous subarray within an array (containing at least one number) which has the largest product.
+
+Example 1:
+
+Input: [2,3,-2,4]
+Output: 6
+Explanation: [2,3] has the largest product 6.
+Example 2:
+
+Input: [-2,0,-1]
+Output: 0
+Explanation: The result cannot be 2, because [-2,-1] is not a subarray.
+"""
+
+from typing import * 
+
+
+def max_product(nums: List[int]) -> int:
+    output = [None for _ in range(len(nums))]
+    for i in range(len(nums)-1, -1, -1):
+        if i == len(nums) - 1:
+            output[i] = nums[i]
+        else:
+            output[i] = max(nums[i], nums[i] * output[i+1])
+    return max(output)
+
+
+def main():
+    assert max_product([2,3,-2,4]) == 6
+    assert max_product([-2,0,-1]) == 0
+
+
+if __name__ == "__main__":
+    main()
